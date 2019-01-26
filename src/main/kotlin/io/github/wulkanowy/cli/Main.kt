756133cad09f0b0aafdf38219c964e28a5f43604
Mikołaj Pich <m.pich@outlook.com>
2019-01-27 00:15:24 +0100

    Add user login

--- src/main/kotlin/io/github/wulkanowy/cli/Main.kt
+++ src/main/kotlin/io/github/wulkanowy/cli/Main.kt
@@ -1,11 +1,35 @@
 package io.github.wulkanowy.cli
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.prompt
+import io.github.wulkanowy.api.Api
+import okhttp3.logging.HttpLoggingInterceptor
 
 class Hello : CliktCommand() {
 
+    private val debug by option().flag()
+
+    private val api = Api()
+
+    private val nick by option().prompt()
+
+    private val pass by option().prompt(hideInput = true)
+
     override fun run() {
-        echo("Hello World!")
+        api.apply {
+            logLevel = if (debug) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+            host = "vulcan.net.pl"
+            email = nick
+            password = pass
+        }
+
+        echo("Ładowanie...")
+
+        val students = api.getStudents().blockingGet()
+        echo("Znaleziono uczniów:")
+        students.forEach { echo("${it.studentName} - ${it.description}") }
     }
 }
 
